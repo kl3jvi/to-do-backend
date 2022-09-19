33ff0d7515c16673b9a2e1910808ac13ac0fe793
kl3jvi <klejvisiper@gmail.com>
2022-09-19 11:22:40 +0200

    heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/to-do-backend/bin/to-do-backend
--- src/main/kotlin/com/kl3jvi/Application.kt
+++ src/main/kotlin/com/kl3jvi/Application.kt
@@ -9,7 +9,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         DatabaseFactory.init()
         configureRouting()
         configureCallLogging()
